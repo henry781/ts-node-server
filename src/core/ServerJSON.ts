543ffbdbcba1d0482142e1b6bf48b0334bde40ea
henry781 <henry.ernst@hotmail.fr>
2018-11-18 12:14:28 +0100

    chore(npm): prepare for first release

--- src/core/ServerJSON.ts
+++ src/core/ServerJSON.ts
@@ -1,20 +1,4 @@
-import {JsonConverter} from 'tipify';
 
 export class ServerJSON {
 
-    private static _tipify = new JsonConverter();
-
-
-    public static serialize(object: any): any {
-
-        try {
-            return ServerJSON.serialize(object);
-        } catch (err) {
-
-        }
-    }
-
-    public static deserialize<T>(json: any, type: any): T {
-
-    }
 }
