dc37d4aeec39fe633405bf9ab06d569129120db9
henry781 <henry.ernst@hotmail.fr>
2019-02-09 14:14:45 +0100

    feat(logging): write time in iso8601

--- src/core/loggerService.ts
+++ src/core/loggerService.ts
@@ -1,11 +1,13 @@
 import * as _pino from 'pino';
+import {LoggerOptions} from 'pino';
 import {environment} from './environment';
 
 const pino = _pino;
 
-export const DEFAULT_LOGGER_OPTIONS = {
+export const DEFAULT_LOGGER_OPTIONS: LoggerOptions = {
     level: environment.LOG_LEVEL,
     prettyPrint: environment.LOG_PRETTY ? {forceColor: true} : undefined,
+    timestamp: () => `,"time":"${new Date().toISOString()}"`,
 };
 
 export const loggerService = pino(DEFAULT_LOGGER_OPTIONS);
