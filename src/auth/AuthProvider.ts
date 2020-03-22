d0cfea54b8d1bb084f88edf5cec478cd5358d39f
henry781 <henry.ernst@hotmail.fr>
2020-03-22 17:32:08 +0100

    fix(auth): add @injectable on AuthProvider

--- src/auth/AuthProvider.ts
+++ src/auth/AuthProvider.ts
@@ -1,4 +1,5 @@
 import {Token} from 'auth-header';
+import {injectable} from 'inversify';
 import {AuthOptions} from '../plugins/common/method/AuthOptions';
 import {Request} from '../types';
 import {Principal} from './Principal';
@@ -6,6 +7,7 @@ import {Principal} from './Principal';
 /**
  * Auth provider
  */
+@injectable()
 export abstract class AuthProvider {
     public abstract async authenticate(request: Request, token: Token, options: AuthOptions): Promise<Principal>;
 }
