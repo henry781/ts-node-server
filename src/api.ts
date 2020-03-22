9cb4b069481534a2f661f3e8b51feedb237169cb
henry781 <henry.ernst@hotmail.fr>
2020-03-22 22:28:35 +0100

    fix(logger): export logger

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,7 @@ export * from './auth/api';
 export * from './client/api';
 export * from './core/api';
 export * from './healthcheck/api';
+export * from './logger/api';
 export * from './mongo/api';
 export * from './plugins/api';
 export * from './test/api';
