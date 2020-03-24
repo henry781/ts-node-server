857676ce877063455808c8e3283d0b382101a3dc
henry781 <henry.ernst@hotmail.fr>
2020-03-24 08:43:36 +0100

    fix(json): upgrade tipify

--- src/core/jsonConverter.ts
+++ src/core/jsonConverter.ts
@@ -1,3 +1,6 @@
 import {JsonConverter as Converter} from 'tipify';
 
-export const jsonConverter = new Converter({unsafe: true}, {tryParse: true, keepObjectFieldValues: true});
+export const jsonConverter = new Converter({
+    deserialize: {tryParse: true, keepObjectFieldValues: true},
+    serialize: {unsafe: true},
+});
