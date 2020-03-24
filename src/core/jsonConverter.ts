70dd5829d6e0f8fce1e0af84416d5895ddade2f3
henry781 <henry.ernst@hotmail.fr>
2020-03-24 17:22:23 +0100

    chore(tipify): keep behavior of tipify 2.0

--- src/core/jsonConverter.ts
+++ src/core/jsonConverter.ts
@@ -1,6 +1,6 @@
 import {JsonConverter as Converter} from 'tipify';
 
 export const jsonConverter = new Converter({
-    deserialize: {tryParse: true, keepObjectFieldValues: true},
+    deserialize: {tryParse: true, keepObjectFieldValues: false},
     serialize: {unsafe: true},
 });
