c289a376449f7c453fb0b77ae14a2501aec3ee75
henry781 <henry.ernst@hotmail.fr>
2022-01-25 21:40:56 +0100

    fix(inversify): add missing decorator

--- src/auth/AuthProvider.ts
+++ src/auth/AuthProvider.ts
@@ -1,4 +1,5 @@
 import {Token} from 'auth-header';
+import {injectable} from 'inversify';
 import {AuthOptions} from '../plugins/common/method/AuthOptions';
 import {Request} from '../types';
 import {Principal} from './Principal';
@@ -6,9 +7,10 @@ import {Principal} from './Principal';
 /**
  * Auth provider
  */
+@injectable()
 export abstract class AuthProvider {
     public abstract authenticate(request: Request, token: Token, options: AuthOptions): Promise<Principal>;
 
-    protected constructor(public name: string) {
+    constructor(public name: string) {
     }
 }
